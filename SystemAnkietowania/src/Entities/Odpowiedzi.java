fbc6cc02c20a25c0a72903b093008207a385e4d2
Adam Olechno <adam.olechno1@gmail.com>
2019-05-13 23:54:56 +0200

    Update Odpowiedzi.java
    change of plans

--- SystemAnkietowania/src/Entities/Odpowiedzi.java
+++ SystemAnkietowania/src/Entities/Odpowiedzi.java
@@ -8,5 +8,19 @@ package Entities;
 @Table(name = "Odpowiedzi")
 public class Odpowiedzi {
 
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @ManyToOne
+        @JoinColumn(name = "ID_Pytania")
+        private int idPytania;
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @ManyToOne
+        @JoinColumn(name = "ID_Ankiety")
+        private int idAnkiety;
+
+        @Column(name = "Odpowiedz")
+        private String odpowiedz;
 
 }
